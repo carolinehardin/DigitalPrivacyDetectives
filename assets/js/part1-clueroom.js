23d4229412568f08b0de47bcec6b73809c2b1c15
samantha murray <60449164+sammurraytuesta@users.noreply.github.com>
2020-12-02 02:25:31 -0800

    added files part1-clueroom .html .js and .css, added jquery file, started work on first digital clue room, opening transition is complete

---
+++ assets/js/part1-clueroom.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+
+    $('.button').click(function(){
+        $('.overlay').fadeOut(2000);
+    });
+    $('.button').click(function(){
+        $('.jumbotron').fadeOut(2000);
+    });
+
+});
